177fbbcc61892478645dc6d324605325b85d8aba
Kelly Lima <keely06@live.com>
2022-02-08 07:47:38 -0300

    plan/apply tests

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,10 +1,16 @@
 terraform {
   required_providers {
-      docker = {
-          source = "kreuzwerker/docker"
-          version = "2.16.0"
-      }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.15.0"
+    }
   }
 }
 
-provider "docker" {}
+provider "docker" {
+    host = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}
