97319bdbc3da532c3a45169bfcc6777b0b8fabe7
Kelly Lima <keely06@live.com>
2022-02-21 12:36:03 -0300

    add lifecycle prevent destroy

--- terraform-docker/container/main.tf
+++ terraform-docker/container/main.tf
@@ -13,4 +13,7 @@ resource "docker_container" "nodered_container" {
 
 resource "docker_volume" "container_volume" {
     name = "${var.name_in}-volume"
+    lifecycle {
+      prevent_destroy = false
+    }
 }
