3c2577daf91126a7e43a88e1bc2647444be8569f
Kelly Lima <keely06@live.com>
2022-02-26 17:04:20 -0300

    add local-exec to create containers.txt

--- terraform-docker/container/main.tf
+++ terraform-docker/container/main.tf
@@ -19,6 +19,13 @@ resource "docker_container" "app_container" {
     container_path = var.container_path_in
     volume_name    = docker_volume.container_volume[count.index].name
   }
+  provisioner "local-exec" {
+    command = "echo ${self.name}: ${self.ip_address}:${join("", [for x in self.ports[*]["external"]: x])} >> containers.txt"
+  }
+  provisioner "local-exec" {
+    when = destroy
+    command = "rm -f containers.txt"
+  }
 }
 
 resource "docker_volume" "container_volume" {
