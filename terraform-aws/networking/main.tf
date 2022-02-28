69fcc09b3f326a2f0f796c132bf0df15b90218d8
Kelly Lima <keely06@live.com>
2022-02-28 17:22:44 -0300

    add public subnets

--- terraform-aws/networking/main.tf
+++ terraform-aws/networking/main.tf
@@ -11,6 +11,20 @@ resource "aws_vpc" "mtc_vpc" {
   enable_dns_support   = true
 
   tags = {
-    Name = "mtc_vpc-${random_integer.random.id}"
+    Name = "mtc_vpc_${random_integer.random.id}"
   }
+}
+
+resource "aws_subnet" "mtc_public_subnet" {
+    # ele vai criar baseado na quantidade que passei de cidrs no root
+    count = length(var.public_cidrs)
+    vpc_id = aws_vpc.mtc_vpc.id
+    # com count.index ele vai pegar o primeiro da lista na primeira iteração, depois o segundo da lista
+    cidr_block = var.public_cidrs[count.index]
+    map_public_ip_on_launch = true
+    availability_zone = ["us-east-1a", "us-east-1b", "us-east-1c"][count.index]
+
+    tags = {
+        Name = "mtc_public_${count.index + 1}"
+    }
 }
