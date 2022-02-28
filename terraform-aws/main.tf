82e3a244fbbc490b2300a249009305218236219d
Kelly Lima <46599722+keelylima@users.noreply.github.com>
2022-02-28 16:16:19 -0300

    Delete main.tf

--- terraform-aws/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
- shared_credentials_file = "˜/.aws/credentials"
- region     = "us-east-1"
-}
-
-resource "aws_instance" "example01" {
- ami           = "ami-0080e4c5bc078760e"
- instance_type = "t2.micro"
-}
