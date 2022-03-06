c06d333ad648331bdf5b0737a4ded42e9b1407fd
Kelly Lima <keely06@live.com>
2022-03-06 16:56:45 -0300

    add route table and igw

--- terraform-aws/networking/main.tf
+++ terraform-aws/networking/main.tf
@@ -38,6 +38,12 @@ resource "aws_subnet" "mtc_public_subnet" {
   }
 }
 
+resource "aws_route_table_association" "mtc_public_assoc" {
+  count = var.public_sn_count
+  subnet_id = aws_subnet.mtc_public_subnet.*.id[count.index]
+  route_table_id = aws_route_table.mtc_public_rt.id
+}
+
 resource "aws_subnet" "mtc_private_subnet" {
   count             = var.private_sn_count
   vpc_id            = aws_vpc.mtc_vpc.id
@@ -48,4 +54,31 @@ resource "aws_subnet" "mtc_private_subnet" {
     Name = "mtc_private_${count.index + 1}"
   }
 
+}
+
+resource "aws_internet_gateway" "mtc_internet_gateway" {
+  vpc_id = aws_vpc.mtc_vpc.id
+  tags = {
+    Name = "mtc_igw"
+  }
+}
+
+resource "aws_route_table" "mtc_public_rt" {
+  vpc_id = aws_vpc.mtc_vpc.id
+  tags = {
+    Name = "mtc_public"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id = aws_route_table.mtc_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.mtc_internet_gateway.id
+}
+
+resource "aws_default_route_table" "mtc_private_rt" {
+  default_route_table_id = aws_vpc.mtc_vpc.default_route_table_id 
+  tags = {
+    Name = "mtc_private"
+  } 
 }
