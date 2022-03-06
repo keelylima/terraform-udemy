8c61bc8fbc023d32069782f069ef39a0d38ec7c9
Kelly Lima <keely06@live.com>
2022-03-06 17:16:08 -0300

    add lifecycle to vpc

--- terraform-aws/networking/main.tf
+++ terraform-aws/networking/main.tf
@@ -22,6 +22,9 @@ resource "aws_vpc" "mtc_vpc" {
   tags = {
     Name = "mtc_vpc_${random_integer.random.id}"
   }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_subnet" "mtc_public_subnet" {
@@ -39,8 +42,8 @@ resource "aws_subnet" "mtc_public_subnet" {
 }
 
 resource "aws_route_table_association" "mtc_public_assoc" {
-  count = var.public_sn_count
-  subnet_id = aws_subnet.mtc_public_subnet.*.id[count.index]
+  count          = var.public_sn_count
+  subnet_id      = aws_subnet.mtc_public_subnet.*.id[count.index]
   route_table_id = aws_route_table.mtc_public_rt.id
 }
 
@@ -71,14 +74,14 @@ resource "aws_route_table" "mtc_public_rt" {
 }
 
 resource "aws_route" "default_route" {
-  route_table_id = aws_route_table.mtc_public_rt.id
+  route_table_id         = aws_route_table.mtc_public_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.mtc_internet_gateway.id
+  gateway_id             = aws_internet_gateway.mtc_internet_gateway.id
 }
 
 resource "aws_default_route_table" "mtc_private_rt" {
-  default_route_table_id = aws_vpc.mtc_vpc.default_route_table_id 
+  default_route_table_id = aws_vpc.mtc_vpc.default_route_table_id
   tags = {
     Name = "mtc_private"
-  } 
+  }
 }
